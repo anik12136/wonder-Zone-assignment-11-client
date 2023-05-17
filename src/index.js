4e87ef28f4eac437c4b6a830f1e36ed8d66f7b82
Mac <mac@Macs-MacBook-Air.local>
2023-05-18 04:05:14 +0600

    initial route set up

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import router from './Routes/Routes';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+reportWebVitals();
