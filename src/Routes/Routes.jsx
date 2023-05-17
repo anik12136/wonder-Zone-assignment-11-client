2de90918a17dc56bf3377e058ebc9413533576ab
Mac <mac@Macs-MacBook-Air.local>
2023-05-18 05:47:19 +0600

    navber and footer added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,49 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import AddAToys from "../Pages/Add-A-Toys/AddAToys";
+import AllToys from "../Pages/All-Toys/AllToys";
+import Blogs from "../Pages/Blogs/Blogs";
+import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Login/Login";
+import MyToys from "../Pages/MyToys/MyToys";
+import SignUp from "../Pages/Registration/SignUp";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }, 
+        {
+            path: 'login', 
+            element: <Login></Login>
+        }, 
+        {
+            path: 'signup', 
+            element: <SignUp></SignUp>
+        },
+        {
+            path: 'allToys', 
+            element: <AllToys></AllToys>
+        },
+        {
+            path: 'myToys', 
+            element: <MyToys></MyToys>
+        },
+        {
+            path: 'addAToys', 
+            element: <AddAToys></AddAToys>
+        },
+        {
+            path: 'blogs', 
+            element: <Blogs></Blogs>
+        },
+        
+      ]
     },
   ]);
 
