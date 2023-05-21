cdc06ff2c0f3774cd7cabe33224fd86499e1e3e2
Mac <mac@Macs-MacBook-Air.local>
2023-05-21 12:01:03 +0600

    added my toys page

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -1,16 +1,17 @@
 
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
+import MytoysRow from './MytoysRow';
 
 const MyToys = () => {
 
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
     console.log(user.email)
     const [myToys, setMyToys] = useState([])
     // const [searchQuery, setSearchQuery] = useState("All");
 
     useEffect(() => {
-        fetch(`https://assignment-11-serve-site-anik12136.vercel.app/allToys/${user?.email}`)
+        fetch(`http://localhost:5000/myToys/${user?.email}`)
             .then(res => res.json())
             .then(data => setMyToys(data))
             .catch(error => console.error(error))
@@ -20,7 +21,36 @@ const MyToys = () => {
 
     return (
         <div>
-            <h2>my toys</h2>
+            <div className="overflow-x-auto mx-20 my-10 ">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>seller</th>
+                            <th>Sub-category</th>
+                            <th>Toy name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            myToys.map(toy => <MytoysRow
+
+                                key={toy._id}
+                                toy={toy}
+                            // handleDelete={handleDelete}
+
+                            ></MytoysRow>)
+                        }
+
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
